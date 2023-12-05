ccb0dc78dbafae4617ea0dd2165929e05ace66cd
Christian Armstrong <christian_armstrong@brown.edu>
2023-12-05 16:54:50 -0500

    feat: upsampling and general synthesis pipeline

--- code/tex_synthesis.py
+++ code/tex_synthesis.py
@@ -1,16 +1,33 @@
+import math
 import numpy as np
 
 
 C = 2 ## number of passes
 S = 2 ## number of subpasses
+DELTA = np.array([[0,0], [0,1], [1,0], [0,1]])
 
-
-def build_gaussian_stack():
+def build_gaussian(img, with_pyramid):
     pass
 
 
-def upsample(S, is_toroidal):
-    pass
+def upsample(S, m, h, with_pyramid):
+    # TODO: check if m should be the full-sized exemplar m or the appropriate pyramid/stack's m
+    new_S = np.zeros(S.shape[0] * 2, S.shape[1] * 2, 2)
+
+    if with_pyramid:
+        new_S[2*S+DELTA] = np.mod(2*S + h*DELTA, m)
+    else:
+        rhs = np.floor(h * np.subtract(DELTA, 0.5))
+        new_S[2*S+DELTA] = np.mod(S + rhs, m)
+        
+    # if we need loops
+    # I, J = np.meshgrid(np.arange(S.shape[0]), np.arange(S.shape[1]))
+    # for i, j in zip(I.flatten(), J.flatten()):
+    #     for delta in np.array([[0,0], [0,1], [1,0], [0,1]]):
+    #         p = S[i, j]
+    #         new_S[2*p+delta] = np.mod(2*S[p]+h*delta, m)
+    
+    return new_S
 
 
 def jitter(S):
