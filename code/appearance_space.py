9740d05d201884b22d1e8ea8f6369d800d0e42c3
Anika Bahl <anikabahl@anikas-mbp-3.devices.brown.edu>
2023-12-10 16:18:22 -0500

    new stacks

--- code/appearance_space.py
+++ code/appearance_space.py
@@ -20,7 +20,7 @@ def get_appearance_space_vector(im, surrounding_size, feature_distance=True):
                 patch = np.concatenate([patch, patch_edges], axis=2)
             patch = np.reshape(patch, (dims * (2 * surrounding_size + 1)**2,))
             vector_im[i - surrounding_size, j - surrounding_size] = patch
-    return useable_im, vector_im
+    return conduct_pca(vector_im)
 
 
 def conduct_pca(image_with_features, desired_dimensions=8):
@@ -29,10 +29,34 @@ def conduct_pca(image_with_features, desired_dimensions=8):
     pca = PCA(n_components=desired_dimensions)
     new_features = pca.fit_transform(image_with_features)
     new_features = np.reshape(new_features, (original_shape[0], original_shape[1], desired_dimensions))
-    return new_features
+    return new_features, pca
+
+def get_neighbors(image_with_features, desired_dimensions=8):
+    height, width, channels = image_with_features.shape
+    neighbor_features = np.zeros((height, width, desired_dimensions * 4))
+    for i in range(height):
+        for j in range(width):
+            dimensions = []
+            if i > 0:
+                if j > 0:
+                    dimensions.append(image_with_features[i - 1, j - 1])
+                if j < width - 1:
+                    dimensions.append(image_with_features[i - 1, j + 1])
+            elif i < height - 1:
+                if j > 0:
+                    dimensions.append(image_with_features[i + 1, j - 1])
+                if j < width - 1:
+                    dimensions.append(image_with_features[i + 1, j + 1])
+            while len(dimensions != 4):
+                dimensions.append(image_with_features[i, j])
+            dimensions = np.array(dimensions)
+            np.reshape(dimensions, (desired_dimensions * 4,))
+            neighbor_features[i, j] = dimensions
+    return conduct_pca(neighbor_features)
 
 
-# im = cv2.imread("data/1.3.1.png", cv2.COLOR_BGR2RGB)
-# im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-# og, vec = get_appearance_space_vector(im, 2)
-# new_features = conduct_pca(vec)
+
+im = cv2.imread("data/texture2.jpg", cv2.COLOR_BGR2RGB)
+im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+og, vec = get_appearance_space_vector(im, 2)
+new_features, m = conduct_pca(vec)
