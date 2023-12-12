930aff35ccf446e85e22da3e755c9c6da23a6f18
lbriody <luke_briody@brown.edu>
2023-12-11 21:18:02 -0500

    done! u ate that up, girlie pop!

--- code/tex_synthesis.py
+++ code/tex_synthesis.py
@@ -25,9 +25,9 @@ def build_param_dict(E, with_pyramid):
     if with_pyramid:
         params['h'] = np.ones(l)
     else:
-        params['h'] = np.power(2, np.arange(start=l, stop=-1, step=-1))
+        params['h'] = np.power(2, np.arange(start=l-1, stop=-1, step=-1))
 
-    params['r'] =  np.power(.75, np.arange(start=l, stop=-1, step=-1))
+    params['r'] =  np.power(.75, np.arange(start=l-1, stop=-1, step=-1))
 
     return params
 
@@ -72,6 +72,7 @@ def gaussian_stack(img, depth=2):
         final_gaussian.append(blur)
     return final_gaussian
 
+
 def upsample(S, m, h, with_pyramid, synth_mode="iso", J=None):
     # TODO: check if m should be the full-sized exemplar m or the appropriate pyramid/stack's m
     new_S = np.zeros((2*S.shape[0], 2*S.shape[1], 2))
@@ -106,57 +107,70 @@ def hash_coords(S, m, l):
     return np.random.rand(S.shape[0], S.shape[1], 2) * (m/(2**(l-1)) - m/(2**l))
 
 
-def isometric_correction(S, Ept, Nt_Ept, pca, near_nbs):
+def isometric_correction(S, Ept, Nt_Ept, pca, near_nbs, m):
     ## compute Ns
     Ns = np.zeros((S.shape[0], S.shape[1], 32))
 
     i, j = np.meshgrid(np.arange(S.shape[0]), np.arange(S.shape[1]), indexing='ij')
     
-    S = np.pad(S, ((1,1), (1,1), (0,0)), mode='constant', constant_values=0).astype(np.int32)
+    S = np.pad(S, ((2,2), (2,2), (0,0)), mode='reflect').astype(np.int32)
+    Ept = np.pad(Ept, ((2,2), (2,2), (0,0)), mode='reflect').astype(np.int32)
     
+    # TODO: figure out what the FAWK is going wrong with upsample/jitter that is breaking this shawty down so bad
+    ## (it's probably the jitter)
+    np.clip(S, 0, 31, out=S)
+
     for k in range(4):
         for l, corr_delta in enumerate(CORR_DELTA):
             for corr_delta_p in CORR_DELTA_PRIME[l]:
-                print((S[i+1 + corr_delta[0] + corr_delta_p[0], j+1 + corr_delta[1] + corr_delta_p[1]]).dtype)
-                Ns[i, j, 8*k:8*(k+1)] += Ept[S[i+1 + corr_delta[0] + corr_delta_p[0], 
-                                               j+1 + corr_delta[1] + corr_delta_p[1]] - corr_delta_p[1]]
+                Ept_idx = S[i+2 + corr_delta[0] + corr_delta_p[0], j+2 + corr_delta[1] + corr_delta_p[1]] - corr_delta_p[1]
+                Ns[i, j, 8*k:8*(k+1)] += Ept[Ept_idx[..., 0] + 2, Ept_idx[..., 1] + 2]
     Ns = Ns / 3
+    Ns = np.reshape(Ns, (-1, 32))
     Ns = pca.transform(Ns)
+    S = S[2:-2, 2:-2]
+    Ept = Ept[2:-2, 2:-2]
     Ns = np.reshape(Ns, (S.shape[0], S.shape[1], 8))
 
-    ## requisite variables
-    corrS = np.ones_like(S) * np.inf
-    corrS = np.pad(corrS, ((1,1), (1,1), (0,0)), mode='constant', constant_values=np.inf)
+    print(S.shape)
 
+    ## requisite variables
     ## subpass time!
     for i in range(SQRT_S):
         for j in range(SQRT_S):
 
             indices = np.stack(np.meshgrid(np.arange(i,S.shape[0],SQRT_S), np.arange(j,S.shape[1],SQRT_S), indexing='ij'), axis=2)
-
-            # ## compute Ns
-            # Ns = np.zeros(S.shape[0], S.shape[1], 32)
-            # interS = np.where(corrS[indices[..., 0] + 1, indices[..., 1] + 1] != np.inf, corrS[indices[..., 0] + 1, indices[..., 1] + 1], S[indices[..., 0], indices[..., 1]])
-            # for i in range(4):
-            #     Ns[..., 8*i:8*(i+1)] = np.sum(Ept[S + CORR_DELTA[..., i, :] + CORR_DELTA_PRIME[..., i, :, :]] - CORR_DELTA_PRIME[..., i, :], axis=2) / 3
-            # Ns = pca.transform(Ns)
-            # Ns = np.reshape(Ns, (S.shape[0], S.shape[1], 8))
-
+            indices_shape = indices.shape
             indices = indices.reshape((-1, 2)) ## is this necessary?
-            all_nbs = indices + SUBPASS_DELTA
-            filled_nbs = np.where(corrS[all_nbs[..., 0] + 1, [..., 1] + 1] != np.inf, all_nbs, np.array([[np.inf,np.inf]])) ## +1 because of padding
-            filled_nbs_coord = np.where(filled_nbs != np.inf, S[filled_nbs[..., 0], filled_nbs[..., 1]], np.array([[np.inf,np.inf]]))
-            filled_near_nbs = np.where(filled_nbs_coord != np.inf, near_nbs[filled_nbs_coord[..., 0], filled_nbs_coord[..., 1]], np.array([[np.inf,np.inf]]))
-            filled_near_nbs = filled_near_nbs - SUBPASS_DELTA
 
-            candidates = np.stack([filled_nbs_coord, filled_near_nbs], axis=2)
-            candidates = np.reshape(candidates, (-1, 2)) ## is this necessary?
+            # TODO: should we compare against S or corrS neighbors once first subpass is complete?
+            candidates = []
+            for delta in SUBPASS_DELTA:
+                all_nbs = indices + delta
+                np.clip(all_nbs, 0, S.shape[0]-1, out=all_nbs)
+                filled_nbs_coord = S[all_nbs[..., 0], all_nbs[..., 1]]
+                filled_near_nbs = near_nbs[filled_nbs_coord[..., 0], filled_nbs_coord[..., 1]]
+                filled_near_nbs = filled_near_nbs - delta
+                filled_nbs_coord = filled_nbs_coord - delta
+                np.clip(filled_near_nbs, 0, m-1, out=filled_near_nbs)
+                np.clip(filled_nbs_coord, 0, m-1, out=filled_nbs_coord)
+                candidates.append(np.stack([filled_nbs_coord, filled_near_nbs], axis=2))
+            
+            candidates = np.array(candidates)
+            candidates = np.swapaxes(candidates, 0, 1)
+            candidates = np.reshape(candidates, (candidates.shape[0], 18, 2))
             candidate_vecs = Nt_Ept[candidates[..., 0], candidates[..., 1]] ## what does this look like?
 
-            distances = np.linalg.norm(candidate_vecs - Ns[indices[..., 0], indices[..., 1]], axis=1) ## check axis
-            min_idx = np.argmin(distances, axis=0) ## check axis
+            differences = candidate_vecs - Ns[indices[..., np.newaxis, 0], indices[..., np.newaxis, 1]]
+            distances = np.linalg.norm(differences, axis=2) ## check axis
+            min_idx = np.argmin(distances, axis=1) ## check axis
+
+            selections = candidates[np.arange(distances.shape[0]), min_idx]
         
-            corrS[indices[..., 0] + 1, indices[..., 1] + 1] = candidates[min_idx]
+            indices = indices.reshape(indices_shape)
+            S[indices[..., 0], indices[..., 1]] = np.reshape(selections, (indices.shape[0], indices.shape[1], 2))
+
+    return S
 
 
 def anisometric_correction(S, E, E_prime, J):
@@ -173,7 +187,7 @@ def anisometric_correction(S, E, E_prime, J):
     pass
 
 
-def synthesize_texture(E, synth_size=256, synth_mode="iso", with_pyramid=False):
+def synthesize_texture(E, synth_size=32, synth_mode="iso", with_pyramid=False):
     E = E.astype(np.float32)
     print("building gaussian stack... or pyramid... but pyramids are for losers...")
     E_stack, with_pyramid = build_gaussian(E, with_pyramid)
@@ -182,26 +196,27 @@ def synthesize_texture(E, synth_size=256, synth_mode="iso", with_pyramid=False):
     print("building appearance space vectors...")
     for E_prime in E_stack:
         E_prime_tilde, _ = get_appearance_space_vector(E_prime, 2)
+        E_prime_tilde = E_prime_tilde.astype(np.float32)
         ASV_stack.append(E_prime_tilde)
     neighbors_stack = []
     nearest_neighbors_stack = []
     print("building neighborhoods and computing nearest neighbors...")
     for E_prime_tilde in ASV_stack:
         nbs, pca = get_neighborhoods(E_prime_tilde)
+        nbs = nbs.astype(np.float32)
         neighbors_stack.append((nbs, pca))
         nearest_neighbors_stack.append(get_nearest_neighbors(nbs))
-
 
     S_i = np.zeros((synth_size, synth_size, 2)).astype(np.int32)
 
     print("beginning coarse to fine synthesis...")
-    for i in range(params['l']+1):
+    for i in range(params['l']):
         h = params['h'][i]
         r = params['r'][i]
         E_prime_tilde = ASV_stack[i]
         nbhds = neighbors_stack[i][0]
         pca = neighbors_stack[i][1]
-        near_nbs = nearest_neighbors_stack[i][0]
+        near_nbs = nearest_neighbors_stack[i]
         print("synthesizing level {}...".format(i))
         print("upsampling...")
         S_i = upsample(S_i, params['m'], h, with_pyramid)
@@ -211,7 +226,7 @@ def synthesize_texture(E, synth_size=256, synth_mode="iso", with_pyramid=False):
             print("correcting...")
             for _ in range(CORR_PASSES):
                 if synth_mode == "iso":
-                    S_i = isometric_correction(S_i, E_prime_tilde, nbhds, pca, near_nbs)
+                    S_i = isometric_correction(S_i, E_prime_tilde, nbhds, pca, near_nbs, params['m'])
                 elif synth_mode == "aniso":
                     S_i = anisometric_correction(S_i, E_prime_tilde)
 
@@ -220,9 +235,9 @@ def synthesize_texture(E, synth_size=256, synth_mode="iso", with_pyramid=False):
 
 if __name__ == "__main__":
     print("reading image...")
-    E = cv2.imread("../data/texture3.png")
+    E = cv2.imread("../data/texture2.jpg")
     E = cv2.cvtColor(E, cv2.COLOR_BGR2RGB)
-    print("read image of size", E.shape)
+    print("read image of size: ", E.shape)
     print("synthesizing texture...")
     S = synthesize_texture(E)
     print("done! u ate that up girlie pop!")
