93cab316f50b9bc5f433bc7932de37681519fc5c
lbriody <luke_briody@brown.edu>
2023-12-11 22:51:32 -0500

    buggy but working

--- code/tex_synthesis.py
+++ code/tex_synthesis.py
@@ -2,9 +2,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 from skimage.filters import gaussian
-from skimage.util import view_as_windows
 from appearance_space import get_appearance_space_vector, get_neighborhoods, get_nearest_neighbors
-from scipy.spatial.distance import euclidean
 from skimage import io
 
 CORR_PASSES = 2
@@ -13,56 +11,27 @@ UPSAMPLE_DELTA = np.array([[0,0], [0,1], [1,0], [0,1]])
 CORR_DELTA = np.array([[1,1], [1,-1], [-1,1], [-1,-1]])
 CORR_DELTA_PRIME = np.array([[[0,0], [1,0], [0,1]], [[0,0], [1,0], [0,-1]], [[0,0], [-1,0], [0,1]], [[0,0], [-1,0], [0,-1]]])
 SUBPASS_DELTA = np.array([[-1, -1], [-1, 0], [-1, 1], [0, -1], [0,0], [0,1], [1, -1], [1, 0], [1, 1]])
-HASH_SEED = 1290 ## seed for jittering
+HASH_SEED = 2 ## seed for jittering
 
 
-def build_param_dict(E, with_pyramid):
+def build_param_dict(E):
     params = {}
+
     params['m'] = E.shape[0]
     l = int(np.log2(params['m']))
     params['l'] = l
     
-    if with_pyramid:
-        params['h'] = np.ones(l)
-    else:
-        params['h'] = np.power(2, np.arange(start=l-1, stop=-1, step=-1))
-
-    params['r'] =  np.power(.75, np.arange(start=l-1, stop=-1, step=-1))
+    params['h'] = np.power(2, np.arange(start=l-1, stop=-1, step=-1))
+    params['r'] = np.power(0.3, np.arange(start=l-1, stop=-1, step=-1))
+    params['c'] = np.repeat([0, 6, 3], [3, 2, l-5])
 
     return params
 
 
-def gaussian_pyramid(img, depth=2, downsample=True):
-    downsampled_img = [img]
-    for i in range(depth):
-        next_img = blur_and_downsample(downsampled_img[-1], downsample=downsample)
-        downsampled_img.append(next_img)
-
-    downsampled_img.reverse()
-
-    return downsampled_img
-
-
-def blur_and_downsample(im, downsample=True):
-    blur = gaussian(im)
-    if downsample:
-        return blur[::2,::2]
-    else: 
-        return blur
-
-
-def build_gaussian(img, with_pyramid):
+def build_gaussian_stack(img):
     img_size = img.shape[0]
     depth = int(np.log2(img_size))
-    if with_pyramid or depth < 5:
-        with_pyramid = True
-        return gaussian_pyramid(img, depth - 1), with_pyramid
-    else:
-        with_pyramid = False
-        return gaussian_stack(img, depth), with_pyramid
 
-
-def gaussian_stack(img, depth=2):
     final_gaussian = []
     for i in range(depth):
         w = 2**(depth - i)
@@ -73,38 +42,33 @@ def gaussian_stack(img, depth=2):
     return final_gaussian
 
 
-def upsample(S, m, h, with_pyramid, synth_mode="iso", J=None):
+def upsample(S, m, h, synth_mode="iso", J=None):
     # TODO: check if m should be the full-sized exemplar m or the appropriate pyramid/stack's m
     new_S = np.zeros((2*S.shape[0], 2*S.shape[1], 2))
 
     if synth_mode == "aniso": # not sure if this is the correct way
         h = J
 
-    if with_pyramid:
-        for delta in UPSAMPLE_DELTA:
-            i, j = np.meshgrid(np.arange(S.shape[0]), np.arange(S.shape[1]), indexing='ij')
-            new_S[2*i+delta[0],2*j+delta[1]] = np.mod(2*S + h*delta, m)
-    else:
-        for delta in UPSAMPLE_DELTA:
-            i, j = np.meshgrid(np.arange(S.shape[0]), np.arange(S.shape[1]), indexing='ij')
-            rhs = np.floor(h * np.subtract(delta, 0.5))
-            new_S[2*i+delta[0], 2*j+delta[1]] = np.mod(S + rhs, m)
+    for delta in UPSAMPLE_DELTA:
+        i, j = np.meshgrid(np.arange(S.shape[0]), np.arange(S.shape[1]), indexing='ij')
+        rhs = np.floor(h * np.subtract(delta, 0.5))
+        new_S[2*i+delta[0], 2*j+delta[1]] = np.mod(S + rhs, m)
         
     return new_S
 
 
-def jitter(S, m, h, r, l):
+def jitter(S, h, r):
     J = np.zeros((S.shape[0], S.shape[1], 2))
-    J = np.floor(h * hash_coords(S, m, l) * r + np.array([[0.5, 0.5]]))
+    J = np.floor(h * hash_coords(S) * r + np.array([[0.5, 0.5]]))
     return S + J
 
 
-def hash_coords(S, m, l):
+def hash_coords(S):
     '''
     Hash function that generates a subpixel shift for each pixel in a matrix
     '''
     np.random.seed(HASH_SEED)
-    return np.random.rand(S.shape[0], S.shape[1], 2) * (m/(2**(l-1)) - m/(2**l))
+    return 2 * np.random.rand(S.shape[0], S.shape[1], 2) - 1
 
 
 def isometric_correction(S, Ept, Nt_Ept, pca, near_nbs, m):
@@ -132,8 +96,6 @@ def isometric_correction(S, Ept, Nt_Ept, pca, near_nbs, m):
     Ept = Ept[2:-2, 2:-2]
     Ns = np.reshape(Ns, (S.shape[0], S.shape[1], 8))
 
-    print(S.shape)
-
     ## requisite variables
     ## subpass time!
     for i in range(SQRT_S):
@@ -149,11 +111,12 @@ def isometric_correction(S, Ept, Nt_Ept, pca, near_nbs, m):
                 all_nbs = indices + delta
                 np.clip(all_nbs, 0, S.shape[0]-1, out=all_nbs)
                 filled_nbs_coord = S[all_nbs[..., 0], all_nbs[..., 1]]
-                filled_near_nbs = near_nbs[filled_nbs_coord[..., 0], filled_nbs_coord[..., 1]]
-                filled_near_nbs = filled_near_nbs - delta
                 filled_nbs_coord = filled_nbs_coord - delta
-                np.clip(filled_near_nbs, 0, m-1, out=filled_near_nbs)
                 np.clip(filled_nbs_coord, 0, m-1, out=filled_nbs_coord)
+                filled_near_nbs = near_nbs[filled_nbs_coord[..., 0], filled_nbs_coord[..., 1]]
+                filled_near_nbs = filled_near_nbs
+                np.clip(filled_near_nbs, 0, m-1, out=filled_near_nbs)
+                
                 candidates.append(np.stack([filled_nbs_coord, filled_near_nbs], axis=2))
             
             candidates = np.array(candidates)
@@ -187,11 +150,11 @@ def anisometric_correction(S, E, E_prime, J):
     pass
 
 
-def synthesize_texture(E, synth_size=32, synth_mode="iso", with_pyramid=False):
+def synthesize_texture(E, synth_size=32, synth_mode="iso"):
     E = E.astype(np.float32)
     print("building gaussian stack... or pyramid... but pyramids are for losers...")
-    E_stack, with_pyramid = build_gaussian(E, with_pyramid)
-    params = build_param_dict(E, with_pyramid)
+    E_stack = build_gaussian_stack(E)
+    params = build_param_dict(E)
     ASV_stack = []
     print("building appearance space vectors...")
     for E_prime in E_stack:
@@ -219,31 +182,38 @@ def synthesize_texture(E, synth_size=32, synth_mode="iso", with_pyramid=False):
         near_nbs = nearest_neighbors_stack[i]
         print("synthesizing level {}...".format(i))
         print("upsampling...")
-        S_i = upsample(S_i, params['m'], h, with_pyramid)
+        S_i = upsample(S_i, params['m'], h)
         print("jittering...")
-        S_i = jitter(S_i, params['m'], h, r, i)
+        S_i = jitter(S_i, h, r)
         if i > 2:
             print("correcting...")
-            for _ in range(CORR_PASSES):
+            for _ in range(params['c'][i]):
                 if synth_mode == "iso":
                     S_i = isometric_correction(S_i, E_prime_tilde, nbhds, pca, near_nbs, params['m'])
                 elif synth_mode == "aniso":
                     S_i = anisometric_correction(S_i, E_prime_tilde)
 
-    return S_i
+    return S_i.astype(np.int32)
+
+
+def convert_coords_to_image(E, S):
+    E_S = np.zeros((S.shape[0], S.shape[1], 3))
+    E_S = E[S[..., 0], S[..., 1]]
+    return E_S
 
 
 if __name__ == "__main__":
     print("reading image...")
-    E = cv2.imread("../data/texture2.jpg")
+    E = cv2.imread("../data/texture3.png")
     E = cv2.cvtColor(E, cv2.COLOR_BGR2RGB)
     print("read image of size: ", E.shape)
     print("synthesizing texture...")
     S = synthesize_texture(E)
     print("done! u ate that up girlie pop!")
-    plt.imshow(S)
+    E_S = convert_coords_to_image(E, S)
+    plt.imshow(E_S)
     plt.show()
     print("saving...")
-    io.imsave("../out/synth_texture2.jpg", S)
+    io.imsave("../out/synth_texture3.png", E_S)
 
 
